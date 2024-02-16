4425f923e97aca96434554da5b13d3f77bace691
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-02-16 13:20:04 -0500

    added jounals route to controller

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const journalController = require("./controllers/journalsController.js");
+
 app.use(express.json());
 app.use(cors());
+app.use("/journal", journalController);
 
 app.get("/", (request, response) => {
   response.status(200).send("<h1>Test Backend Server<h1>");
